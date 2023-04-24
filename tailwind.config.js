b9910337c7b18ad4d46a9cc8c896d58433640927
maurimartinezz <martinezmauriciojulian@gmail.com>
2023-04-24 06:30:26 -0300

    create project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const colors = require("tailwindcss/colors");
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        success: colors.green,
+        primary: colors.blue,
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
