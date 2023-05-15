060fca744f2ed30cd3621288e453617031071b73
Mauricio Martinez <mjmartinez@hospitalaleman.com>
2023-05-15 12:59:58 -0300

    llamdo api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -12,6 +13,7 @@ import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BoardComponent } from './pages/board/board.component';
+import { ScrollComponent } from './pages/scroll/scroll.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,7 @@ import { BoardComponent } from './pages/board/board.component';
     BoardsComponent,
     NavbarComponent,
     BoardComponent,
+    ScrollComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,6 +32,7 @@ import { BoardComponent } from './pages/board/board.component';
     FontAwesomeModule,
     CdkAccordionModule,
     DragDropModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
